90ab0a7b02e047ca6c6eb2340dd661287652b3c0
Heather <Heather@cynede.net>
2013-08-23 09:55:56 +0400

    enable git prompt

--- config.fish
+++ config.fish
@@ -1,1 +1,7 @@
 set -U EDITOR emacsclient
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch green
